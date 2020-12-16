900a5f0e922a085b3bd38f09327b510664740ff4
borislavstoychev <stoy4ew@gmail.com>
2020-12-16 21:14:30 +0200

    error folder

--- soft_uni_fundamentals/Regular Expressions/more ecercise/03_match_dates.py
+++
@@ -1,7 +0,0 @@
-import re
-
-date = input()
-pattern = r"\b(?P<day>\d{2})([-.\/])(?P<month>[A-Z][a-z]{2})\2(?P<year>\d{4}\b)"
-matches = re.findall(pattern, date)
-for match in matches:
-    print(f"Day: {match[0]}, Month: {match[2]}, Year: {match[3]}")
--- soft_uni_fundamentals/Regular Expressions/more ecercise/04_match_numbers.py
+++
@@ -1,7 +0,0 @@
-import re
-
-line = input()
-pattern = r"(^|(?<=\s))-?\d+(\.\d+)?($|(?=\s))"
-matches = re.finditer(pattern, line)
-for match in matches:
-    print(match.group(0), end=" ")
