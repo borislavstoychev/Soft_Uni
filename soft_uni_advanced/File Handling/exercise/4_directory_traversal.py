bf53849bc2098c21e6d06b55b54a38121296ae94
borislavstoychev <borislav_stoychev@abv.bg>
2021-02-01 00:22:25 +0200

    file handling exercise

--- soft_uni_advanced/File Handling/exercise/4_directory_traversal.py
+++ soft_uni_advanced/File Handling/exercise/4_directory_traversal.py
@@ -18,6 +18,11 @@ def report_files_extensions(files):
 dir_content = os.listdir()
 files = extract_fails(dir_content)
 report_info = report_files_extensions(files)
+result = ""
 for key, value in sorted(report_info.items(), key=lambda el: el[0]):
-    print(f".{key}")
-    print(*[f"--- {v}"for v in value], sep="\n")
+    result += f".{key}\n"
+    for v in value:
+        result += f"- - - {v}.{key}\n"
+with open('C:\\Users\\Kofto\\OneDrive\\Работен плот\\my_report_result.txt', "w") as file:
+    file.write(result)
+
