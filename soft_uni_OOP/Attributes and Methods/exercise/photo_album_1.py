61b32b3b643ddda7e31f55554206a394f580cbed
borislavstoychev <stoy4ew@gmail.com>
2021-03-01 12:23:02 +0200

    error fixed

--- soft_uni_OOP/Attributes and Methods/exercise/photo_album_1.py
+++ soft_uni_OOP/Attributes and Methods/exercise/photo_album_1.py
@@ -5,40 +5,79 @@ class PhotoAlbum:
         self.photos = [[]for i in range(pages)]
         self.start = 0
 
-    @staticmethod
+    @classmethod
     def from_photos_count(cls, photos_count: int):
         return cls(photos_count // 4)
 
     def add_photo(self, label: str):
-        if self.start not in range(self.pages):
-            return "No more free spots"
         if len(self.photos[self.start]) == 4:
             self.start += 1
+        if self.start not in range(self.pages):
+            return "No more free spots"
         self.photos[self.start].append(label)
         return f"{label} photo added successfully on page {self.start + 1} slot {len(self.photos[self.start])}"
 
     def display(self):
-        result = [f'{"-" * 11}\n' + len(self.photos[i]) * "[] ".rstrip() for i in range(self.pages) if len(self.photos[i]) != 0]
-        return "\n".join(result) + "\n" f'{"-" * 11}'
+        result = ""
+        for page in self.photos:
+            if len(page) != 0:
+                result += f'{"-" * 11}\n' + len(page) * "[] "
+                if not page == self.photos[-1]:
+                    result = result[: -1] + "\n"
+                else:
+                    result = result[: -1]
+            else:
+                result += f'{"-" * 11}\n'
+        return result + f'\n{"-" * 11}\n'
 
 
+import unittest
 
 
+class TestsPhotoAlbum(unittest.TestCase):
+    def test_init_creates_all_attributes(self):
+        album = PhotoAlbum(2)
+        self.assertEqual(album.pages, 2)
+        self.assertEqual(album.photos, [[], []])
+
+    def test_from_photos_should_create_instace(self):
+        album = PhotoAlbum.from_photos_count(12)
+        self.assertEqual(album.pages, 3)
+        self.assertEqual(album.photos, [[], [], []])
+
+    def test_add_photo_with_no_free_spots(self):
+        album = PhotoAlbum(1)
+        album.add_photo("baby")
+        album.add_photo("first grade")
+        album.add_photo("eight grade")
+        album.add_photo("party with friends")
+        result = album.add_photo("prom")
+        self.assertEqual(result, "No more free spots")
+
+    def test_add_photo_with_free_spots(self):
+        album = PhotoAlbum(1)
+        album.add_photo("baby")
+        album.add_photo("first grade")
+        album.add_photo("eight grade")
+        album.add_photo("party with friends")
+        self.assertEqual(album.photos, [['baby', 'first grade', 'eight grade', 'party with friends']])
+
+    def test_display_with_one_page(self):
+        album = PhotoAlbum(1)
+        album.add_photo("baby")
+        album.add_photo("first grade")
+        album.add_photo("eight grade")
+        album.add_photo("party with friends")
+        result = album.display()
+        self.assertEqual(result, "-----------\n[] [] [] []\n-----------\n")
+
+    def test_display_with_three_pages(self):
+        album = PhotoAlbum(3)
+        for _ in range(8):
+            album.add_photo("asdf")
+        result = album.display()
+        self.assertEqual(result, "-----------\n[] [] [] []\n-----------\n[] [] [] []\n-----------\n\n-----------\n")
 
 
-
-album = PhotoAlbum(4)
-
-print(album.add_photo("baby"))
-print(album.add_photo("first grade"))
-print(album.add_photo("eight grade"))
-print(album.add_photo("party with friends"))
-print(album.add_photo("baby"))
-print(album.add_photo("first grade"))
-print(album.add_photo("eight grade"))
-print(album.add_photo("party with friends"))
-print(album.photos)
-print(album.add_photo("prom"))
-print(album.add_photo("wedding"))
-
-print(album.display())
+if __name__ == "__main__":
+    unittest.main()
