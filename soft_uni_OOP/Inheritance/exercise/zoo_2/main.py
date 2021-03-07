10bffcfbc8e4218add28ee6044529acc4b285f3f
borislavstoychev <stoy4ew@gmail.com>
2021-03-07 19:54:38 +0200

    zoo 02

---
+++ soft_uni_OOP/Inheritance/exercise/zoo_2/main.py
@@ -0,0 +1,18 @@
+from project.animal import Animal
+from project.mammal import Mammal
+from project.reptile import Reptile
+from project.lizard import Lizard
+from project.bear import Bear
+from project.snake import Snake
+from project.gorilla import Gorilla
+
+
+if __name__ == "__main__":
+    mammal = Mammal("Stella")
+    print(mammal.__class__.__bases__[0].__name__)
+    print(mammal.name)
+    print(mammal._Animal__name)
+    lizard = Lizard("John")
+    print(lizard.__class__.__bases__[0].__name__)
+    print(lizard.name)
+    print(lizard._Animal__name)
