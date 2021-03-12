bf033262e0af982b3d2bd604491d0e260c647b21
borislavstoychev <stoy4ew@gmail.com>
2021-03-12 19:41:31 +0200

    added example

--- soft_uni_OOP/SOLID/lab/books.py
+++ soft_uni_OOP/SOLID/lab/books.py
@@ -57,4 +57,15 @@ r = Reading("Borkata", "Stoychev", l.find_book("criminal"))
 r.turn_page(254)
 print(r)
 
+# BEFORE
+# class Book:
+#     def __init__(self, title, author, location):
+#         self.title = title
+#         self.author = author
+#         self.location = location
+#         self.page = 0
+#
+#     def turn_page(self, page):
+#         self.page = page
+
 
