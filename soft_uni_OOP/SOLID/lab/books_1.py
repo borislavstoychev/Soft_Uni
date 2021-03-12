1b2943ec4def8f5af4d8125af368846cf542d4e2
borislavstoychev <stoy4ew@gmail.com>
2021-03-12 17:07:20 +0200

    some changes

--- soft_uni_OOP/SOLID/lab/books_1.py
+++ soft_uni_OOP/SOLID/lab/books_1.py
@@ -1,17 +1,21 @@
+from abc import ABC, abstractmethod
+
+
 class Book:
 
-    def __init__(self, title, author, pages):
+    def __init__(self, title, author, page=0):
         self.title = title
         self.author = author
-        self.pages = pages
+        self.page = page
 
     def __repr__(self):
-        return f"Author: {self.author}, Tittle: {self.title}, Pages: {self.pages} "
+        return f"Author: {self.author}, Tittle: {self.title}"
 
 
 class Library:
 
-    def __init__(self):
+    def __init__(self, location):
+        self.location = location
         self.books = []
 
     def find_book(self, title):
@@ -24,13 +28,33 @@ class Library:
         self.books.append(book)
 
 
+class Reader(ABC):
 
-b = Book("criminal", "Bobby", 999)
-l = Library()
+    def __init__(self, name, surname, book: Book):
+        self.name = name
+        self.surname = surname
+        self.book = book
+
+    @abstractmethod
+    def __repr__(self):
+        ...
+
+
+class Reading(Reader):
+
+    def turn_page(self, page):
+        self.book.page = page
+
+    def __repr__(self):
+        return f"{self.name} {self.surname} reading page {self.book.page} '{self.book}'"
+
+
+
+b = Book("criminal", "Bobby")
+l = Library("Petrich")
 l.add_book(b)
-print(l.find_book("criminal"))
-
-
-
+r = Reading("Borkata", "Stoychev", l.find_book("criminal"))
+r.turn_page(254)
+print(r)
 
 
