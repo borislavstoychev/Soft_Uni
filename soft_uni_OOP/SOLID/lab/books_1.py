7c5844ab6cbf8a09841d34e91d346054a1903abb
borislavstoychev <stoy4ew@gmail.com>
2021-03-12 12:00:13 +0200

    books 01

---
+++ soft_uni_OOP/SOLID/lab/books_1.py
@@ -0,0 +1,36 @@
+class Book:
+
+    def __init__(self, title, author, pages):
+        self.title = title
+        self.author = author
+        self.pages = pages
+
+    def __repr__(self):
+        return f"Author: {self.author}, Tittle: {self.title}, Pages: {self.pages} "
+
+
+class Library:
+
+    def __init__(self):
+        self.books = []
+
+    def find_book(self, title):
+        searching_book = [book for book in self.books if book.title == title]
+        if searching_book:
+            return searching_book[0]
+        return
+
+    def add_book(self, book: Book):
+        if book not in self.books:
+            self.books.append(book)
+
+
+b = Book("criminal", "Bobby", 999)
+l = Library()
+l.add_book(b)
+print(l.find_book("criminal"))
+
+
+
+
+
