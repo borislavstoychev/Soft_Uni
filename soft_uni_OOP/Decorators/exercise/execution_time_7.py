f418887d96bfb2c632ae28507dde49a91170d8e0
borislavstoychev <stoy4ew@gmail.com>
2021-03-18 14:54:23 +0200

    Execution Time 07

---
+++ soft_uni_OOP/Decorators/exercise/execution_time_7.py
@@ -0,0 +1,32 @@
+from timeit import default_timer as timer
+
+
+def exec_time(func):
+    def wrapper(*args):
+        start_time = timer()
+        func(*args)
+        end_time = timer()
+        return end_time - start_time
+    return wrapper
+
+
+@exec_time
+def loop(start, end):
+    total = 0
+    for x in range(start, end):
+        total += x
+    return total
+
+
+@exec_time
+def concatenate(strings):
+    result = ""
+    for string in strings:
+        result += string
+    return result
+
+
+print(concatenate(["a" for i in range(1000000)]))
+
+
+print(loop(1, 10000000))
